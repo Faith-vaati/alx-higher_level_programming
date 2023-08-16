b43bd8d8840f2872f379f9c09f3d57b6d24bc79f
Faith-vaati <vaati.faith79@gmail.com>
2023-08-15 20:16:56 -0700

    correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    return(my_string.translate({ord('C'): None, ord('c'): None}))
+    new_str = [x for x in my_string if x not in 'c''C']
+    return "".join(new_str)
