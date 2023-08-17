e39500b20cbe112e4a8e1246ab7de5052b079846
Faith-vaati <vaati.faith79@gmail.com>
2023-08-16 20:02:43 -0700

    9 fix

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-
 def multiply_by_2(a_dictionary):
-        dictionary = a_dictionary.copy()
-        dictionary.update((key, value*2) for key, value in dictionary.items())
-        return dictionary
+    new_directory = {}
+    for i in a_dictionary:
+        new_directory[i] = a_dictionary[i] * 2
+    return new_directory
