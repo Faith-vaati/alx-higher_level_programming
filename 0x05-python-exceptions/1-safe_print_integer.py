bda44675ea5cd62ec68cfa53646e079439ee98e4
Faith-vaati <vaati.faith79@gmail.com>
2023-08-28 19:36:57 -0700

    safe print

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
+def safe_print_integer(value):
     try:
-        i = 0
-        for each in my_list:
-            if i < x:
-                print("{}".format(each), end='')
-                i += 1
-        print()
-        return i
-    except SyntaxError:
-        print("Error in your code syntax")
+        print("{:d}".format(value))
+        return True
+    except:
+        return False
