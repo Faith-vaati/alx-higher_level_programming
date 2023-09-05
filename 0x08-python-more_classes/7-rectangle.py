658c5b27c1cad2308f9be192562ae949f8cf4700
Faith-vaati <vaati.faith79@gmail.com>
2023-09-05 06:19:10 -0700

    fix

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,66 +1,99 @@
 #!/usr/bin/python3
+"""
+Class Rectangle
+"""
+
+
 class Rectangle:
     number_of_instances = 0
     print_symbol = '#'
-
+    """
+    Init method to set width and height variables.
+    """
     def __init__(self, width=0, height=0):
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
-    def __str__(self):
-        string = ""
-        if (self.width is 0 or self.__height is 0):
-            return ""
-        for i in range(self.__height):
-            for x in range(self.__width):
-                try:
-                    string += str(self.print_symbol)
-                except Exception:
-                    string += type(self).print_symbol
-            if (i != self.height - 1):
-                string = string + "\n"
-        return string
-
-    def __repr__(self):
-        return ('{self.__class__.__name__}({}, {})'.
-                format(self.__width, self.__height, self=self))
-
-    @property
-    def width(self):
-        return (self.__width)
-
+    """
+    Getter Heigth
+    """
     @property
     def height(self):
-        return (self.__height)
+        return self.__height
 
-    @width.setter
-    def width(self, value):
-        if (type(value) is not int):
-            raise TypeError("width must be an integer")
-        elif (value < 0):
-            raise ValueError("width must be >= 0")
-        else:
-            self.__width = value
-
+    """
+    Setter Height
+    """
     @height.setter
     def height(self, value):
-        if (type(value) is not int):
-            raise TypeError("width must be an integer")
-        elif (value < 0):
-            raise ValueError("width must be >= 0")
-        else:
+        if type(value) == int and value >= 0:
             self.__height = value
+        elif type(value) != int:
+            raise NameError("height must be an integer")
+        elif value < 0:
+            raise NameError("height must be >= 0")
 
+    """
+    Getter Width
+    """
+    @property
+    def width(self):
+        return self.__width
+
+    """
+    Setter Width
+    """
+    @width.setter
+    def width(self, value):
+        if type(value) == int and value >= 0:
+            self.__width = value
+        elif type(value) != int:
+            raise NameError("width must be an integer")
+        elif value < 0:
+            raise NameError("width must be >= 0")
+
+    """
+    Area
+    """
     def area(self):
-        return (self.width * self.__height)
+        return self.__height*self.__width
 
+    """
+    Perimeter
+    """
     def perimeter(self):
-        if (self.width is 0 or self.__height is 0):
-            return 0
+        if self.__height == 0 or self.__width == 0:
+            self.__perimeter = 0
         else:
-            return (2 * (self.width + self.__height))
+            self.__perimeter = 2 * (self.__height + self.__width)
+        return self.__perimeter
 
+    """
+    Print rectangle as string and user readability
+    """
+    def __str__(self):
+        square = []
+        if self.__width and self.__height:
+            for i in range(0, self.__height):
+                for j in range(self.__width):
+                    square.append(str(self.print_symbol))
+                if j <= self.__width:
+                    square.append('\n')
+            return "".join(square[:-1])
+        else:
+            square = ''
+            return square
+
+    """
+    Print rectangle info for programming details
+    """
+    def __repr__(self):
+        return "Rectangle" + "(" + str(self.__width)\
+            + ", " + str(self.__height) + ")"
+    """
+    Print message when deleting an instance
+    """
     def __del__(self):
-        print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
