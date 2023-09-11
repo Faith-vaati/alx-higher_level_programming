8bb635515f4302f83f2841c04799e62483a0df07
Faith-vaati <vaati.faith79@gmail.com>
2023-09-11 02:56:12 -0700

    inherits from

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Check if instance is inherited
+Function that verify if the object is an instance of a class that
+inherited (directly or indirectly) from the specified class
 """
 
 
 def inherits_from(obj, a_class):
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) != a_class
