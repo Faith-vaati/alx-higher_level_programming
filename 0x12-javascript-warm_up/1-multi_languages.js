e888d03557079f2c8dd01f8d7fcf9f375d220ed4
Faith-vaati <vaati.faith79@gmail.com>
2023-12-19 06:21:07 -0800

    task 1 fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const onC = 'C is fun';
-const onP = 'Python is cool';
-const onJ = 'JavaScript is amazing';
-console.log(onC);
-console.log(onP);
-console.log(onJ);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
