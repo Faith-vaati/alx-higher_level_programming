277e4a41a2b9bad47259e83ef9b2da9e175606cd
Faith-vaati <vaati.faith79@gmail.com>
2024-02-27 06:32:06 -0800

    create db fix

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,22 @@
  -- create database
-CREATE DATABASE hbtn_0c_0;
+DELIMITER //
+
+CREATE PROCEDURE create_database_if_not_exists()
+BEGIN
+	DECLARE db_count INT;
+	
+	-- Check if the database exists
+    	SET db_count = (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0');
+
+    	-- If the database doesn't exist, create it
+    	IF db_count = 0 THEN
+		CREATE DATABASE hbtn_0c_0;
+		SELECT 'Database hbtn_0c_0 created successfully.' AS Message;
+	ELSE
+		SELECT 'Database hbtn_0c_0 already exists.' AS Message;
+END IF;
+	END//
+	DELIMITER ;
+
+	-- Call the stored procedure
+	CALL create_database_if_not_exists();
