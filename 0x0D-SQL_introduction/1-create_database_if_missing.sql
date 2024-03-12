3075577cc8395e1d352249d62513818a1b43105d
Faith-vaati <vaati.faith79@gmail.com>
2024-03-12 04:50:09 -0700

    database create

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,22 +1,2 @@
- -- create database
-DELIMITER //
-
-CREATE PROCEDURE create_database_if_not_exists()
-BEGIN
-	DECLARE db_count INT;
-	
-	-- Check if the database exists
-    	SET db_count = (SELECT COUNT(*) FROM information_schema.schemata WHERE schema_name = 'hbtn_0c_0');
-
-    	-- If the database doesn't exist, create it
-    	IF db_count = 0 THEN
-		CREATE DATABASE hbtn_0c_0;
-		SELECT 'Database hbtn_0c_0 created successfully.' AS Message;
-	ELSE
-		SELECT 'Database hbtn_0c_0 already exists.' AS Message;
-END IF;
-	END//
-	DELIMITER ;
-
-	-- Call the stored procedure
-	CALL create_database_if_not_exists();
+-- creates the database hbtn_0c_0 in your MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
