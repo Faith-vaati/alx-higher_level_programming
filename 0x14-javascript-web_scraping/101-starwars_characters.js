1dc97aedeb9cf22b00af017129f9fbbc28294caf
Faith-vaati <vaati.faith79@gmail.com>
2024-05-28 09:46:50 +0300

    Task 100 and 101 fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.dev/api/films/' + process.argv[2];
+const request = require("request");
+const url = "https://swapi-api.hbtn.io/api/films/" + process.argv[2];
 request(url, function (error, response, body) {
   if (!error) {
-    let characters = JSON.parse(body).characters;
+    const characters = JSON.parse(body).characters;
     printCharacters(characters, 0);
   }
 });
